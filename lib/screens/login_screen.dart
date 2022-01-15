9ebcc41f4afbbb066c8a3010a3b8c9278bd6cb13
thanhnguyen98 <ngtruongthanh98@gmail.com>
2022-01-15 14:33:36 +0700

    Login Screen UI (Mobile)

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/utils/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
@@ -19,8 +21,16 @@ class _LoginScreenState extends State<LoginScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               // svg image
+              SvgPicture.asset(
+                'assets/ic_instagram.svg', 
+                color: primaryColor, 
+                height: 64,
+              ),
+              const SizedBox(height: 64),
               // Text field input for email
+
               // Text field input for password
+
               // Button for login
               // Transitions to the signup screen
             ],
